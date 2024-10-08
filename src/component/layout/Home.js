f53569e895e68c638878145e69b7874367e1d338
ruri <sub08041@naver.com>
2024-10-09 03:30:37 +0900

    feat 내 정보 수정 페이지 추가

--- src/component/layout/Home.js
+++ src/component/layout/Home.js
@@ -1,30 +1,31 @@
-import React from "react";
-import Slider from "react-slick";
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick-theme.css';
 import '../css/Font.css';
 import LeftArrow from '../images/Left_arrow.png'; // 좌측 화살표 이미지 경로
 import RightArrow from '../images/Right_arrow.png'; // 우측 화살표 이미지 경로
-import Member from "../page/02_Member";
-import Team from "../page/02_Team";
-import Match from "../page/02_Match";
-import 챙 from "../images/챙.jpg";
+import Member from '../page/02_Member';
+import Team from '../page/02_Team';
+import Match from '../page/02_Match';
+import 챙 from '../images/챙.jpg';
 
 export default function Home() {
     const MatchRenderContainer = () => {
-        <Match/>
-        console.log("매칭 렌더링");
-    }
+        console.log('매칭 렌더링');
+        return <Match />;
+    };
     const TeamRenderContainer = () => {
-        <Team/>
-        console.log("팀 렌더링");
-    }
+        console.log('팀 렌더링');
+        return <Team />;
+    };
     const MemberRenderContainer = () => {
-        <Member/>
-        console.log("멤버 렌더링");
-    }
+        console.log('멤버 렌더링');
+        return <Member />;
+    };
+
     const settings = {
         dots: true,
         slidesToScroll: 1,
@@ -34,9 +35,9 @@ export default function Home() {
         slidesToShow: 1,
         arrows: true,
         autoplay: true,
-        autoplaySpeed : 5000,
+        autoplaySpeed: 5000,
         prevArrow: <Arrow src={LeftArrow} alt="Previous" />,
-        nextArrow: <Arrow src={RightArrow} alt="Next" />
+        nextArrow: <Arrow src={RightArrow} alt="Next" />,
     };
 
     return (
@@ -47,72 +48,59 @@ export default function Home() {
                         <Image src={챙} alt="팀원 구하기" />
                     </Navi>
                 </SliderContainer>
-                <SliderContainer>
-                    두번째 화면
-                </SliderContainer>
-                <SliderContainer>
-                    세번째 화면
-                </SliderContainer>
-                <SliderContainer>
-                    네번째 화면
-                </SliderContainer>
-                <SliderContainer>
-                    다섯번째 화면
-                </SliderContainer>
-                <SliderContainer>
-                    여섯번째 화면
-                </SliderContainer>
+                <SliderContainer>두번째 화면</SliderContainer>
+                <SliderContainer>세번째 화면</SliderContainer>
+                <SliderContainer>네번째 화면</SliderContainer>
+                <SliderContainer>다섯번째 화면</SliderContainer>
+                <SliderContainer>여섯번째 화면</SliderContainer>
             </Slider>
             <RenderContainer>
-                <BulletinContainer>{MatchRenderContainer()}</BulletinContainer>
                 <BulletinContainer>{TeamRenderContainer()}</BulletinContainer>
                 <BulletinContainer>{MemberRenderContainer()}</BulletinContainer>
-                </RenderContainer>
+                <BulletinContainer>{MatchRenderContainer()}</BulletinContainer>
+            </RenderContainer>
         </Container>
     );
 }
 
 const Container = styled.div`
     padding: 50px 300px;
-    position: relative; /* 자식 요소의 위치를 기준으로 설정 */
+    position: relative;
     max-width: 100%;
 `;
 const SliderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: auto;
-  max-width: 100%;
-  font-size: 24px;
-  text-align: center; 
-  font-family: Ganpan;  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    max-width: 100%;
+    font-size: 24px;
+    text-align: center;
+    font-family: Ganpan;
 `;
-
 const Arrow = styled.img`
-  width: 30px; 
-  height: 30px;
-  cursor: pointer;
-  z-index: 1; 
-  position: absolute; 
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    z-index: 1;
+    position: absolute;
 `;
 const RenderContainer = styled.div`
-    margin-top : 4%;
+    margin-top: 4%;
     display: flex;
-    width:90%;
+    width: 90%;
     margin: 4% auto 0 auto;
 `;
 const BulletinContainer = styled.div`
-    flex:1;
-    width:30%;
+    flex: 1;
+    width: 30%;
     aspect-ratio: 1;
 `;
-
 const Navi = styled(NavLink)`
     display: flex;
     justify-content: center;
-    align-item: center;
+    align-items: center; /* align-item -> align-items로 수정 */
 `;
-
 const Image = styled.img`
     width: 90%;
     height: auto;
--- src/component/page/03_Account.js
+++ src/component/page/03_Account.js
@@ -1,23 +1,78 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-export default function Account(){
-    return(
+export default function Account() {
+    return (
         <Container>
-            <LineContainer><TextContainer>이름</TextContainer><InputContainer type="text"></InputContainer></LineContainer>
+            <LineContainer>
+                <TextContainer>이름</TextContainer>
+                <InputContainer type="text" />
+            </LineContainer>
+            <LineContainer>
+                <TextContainer>포지션</TextContainer>
+                <InputContainer type="text" />
+            </LineContainer>
+            <LineContainer>
+                <TextContainer>이메일</TextContainer>
+                <InputContainer type="text" />
+            </LineContainer>
+            <LineContainer>
+                <TextContainer>전화번호</TextContainer>
+                <InputContainer type="text" />
+            </LineContainer>
+            <LineContainer>
+                <TextContainer>자기소개</TextContainer>
+                <InputContainer type="text" />
+            </LineContainer>
+            <ButtonContainer>
+                <SubmitButton>제출</SubmitButton>
+            </ButtonContainer>
         </Container>
-    )
+    );
 }
 
 const Container = styled.div`
-    display:flex;
-    flex-direction:column;
+    display: flex;
+    flex-direction: column;
 `;
+
 const LineContainer = styled.div`
-    display:flex;
-`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    margin-bottom: 5px;
+`;
+
 const TextContainer = styled.div`
+    width: 30%;
+    margin-right: 5px;
+    font-size: 20px;
+    text-align: right;
+`;
 
-`
 const InputContainer = styled.input`
+    width: 70%;
+    border: 1px black solid;
+    border-radius: 5px;
+    padding: 5px;
+    font-size: 18px;
+`;
 
-`
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
+`;
+
+const SubmitButton = styled.button`
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 18px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #45a049;
+    }
+`;
