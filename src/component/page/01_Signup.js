ccac542cc8474b1e45ded0be7ece49ce067ecd63
ruri <sub08041@naver.com>
2024-12-03 17:54:34 +0900

    머지용커밋

--- src/component/page/01_Signup.js
+++ src/component/page/01_Signup.js
@@ -21,7 +21,7 @@ export default function Signup() {
         if (id && password) {
             try {
                 const response = await axios.post(
-                    'https://3.34.133.247:8080/user/register',
+                    'https://3.34.133.247/user/register',
                     {
                         user_name: username,
                         nickname: id,
